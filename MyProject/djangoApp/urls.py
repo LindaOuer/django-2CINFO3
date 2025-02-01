c5357b65091beafb5fd925048d5320eeb74512f3
LindaOuer <linda.ouerfelli@gmail.com>
2025-02-01 17:15:34 +0100

    Admin example 1

--- MyProject/djangoApp/urls.py
+++ MyProject/djangoApp/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('home/', index, name='app_index'),
     path('list/', listStudents, name='app_listStudents'),
-    path('details/<int:id>', detailsStudent, name='app_listStudents'),
+    path('details/<int:id>', detailsStudent, name='app_detailStudent'),
 ]
